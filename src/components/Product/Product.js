600559586eb2257fb52eec1a8f37de98987c2ffc
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-25 11:42:36 +0600

    buttton added & fontawesm

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+import './Product.css'
+
 
 const Product = (props) => {
+  console.log(props.product)
+  const { img, name, seller, stock, price } = props.product;
   return (
-    <div>
-      
-      <h4>{props.product.name}</h4>
-      <h4>{props.product.name}</h4>
-      
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h4 className='product-name'>{name}</h4>
+
+        <p><small>By: {seller}</small></p>
+        <p><small>{price}</small></p>
+        <p><small>By:Only {stock} left in stock-order soon</small></p>
+        <button className='my-btn'><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+
+
+      </div>
     </div>
   );
 };
